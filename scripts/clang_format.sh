64f45decc9564e9e727796bec3eed05915320181
comapi <comapi0905@gmail.com>
2024-10-25 15:13:50 +0900

    Add: inplace option to clang_format.sh

--- scripts/clang_format.sh
+++ scripts/clang_format.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-clang-format --dry-run --Werror ./src/sample.cpp
+DEFAULT_OPTION="--dry-run"
+
+if [[ $# -gt 0 && $1 == "-i" ]]; then
+    DEFAULT_OPTION="-i"
+fi
+
+clang-format $DEFAULT_OPTION --Werror ./src/sample.cpp
