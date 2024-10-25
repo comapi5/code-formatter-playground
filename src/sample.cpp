17a3b019cf2ca65a381b42f9e7607166460b2378
comapi <comapi0905@gmail.com>
2024-10-25 16:09:15 +0900

    Refactor: format sample.cpp

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int main() {
-    int a, b, sum;
-    a=5;
-    b=8;
-    
-        sum = a + b;
+  int a, b, sum;
+  a = 5;
+  b = 8;
 
-    std::cout << "Sum: " << sum << std::endl;
+  sum = a + b;
 
-    return 0;
+  std::cout << "Sum: " << sum << std::endl;
+
+  return 0;
 }
